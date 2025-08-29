0116f200f39b5327cff991728767fe2a9a182e7f
internsujank <internsujank@unicourt.com>
2025-08-29 17:09:35 +0530

    Workshop2 completion

--- myworld/members/models.py
+++ myworld/members/models.py
@@ -7,6 +7,7 @@ BRANCH_CHOICES = (
     ("CA", "CA"),
 )
 
+
 # Create your models here.
 class Students(models.Model):
     first_name = models.CharField(max_length=200)
@@ -14,7 +15,7 @@ class Students(models.Model):
     address = models.CharField(max_length=200)
     roll_number = models.IntegerField()
     mobile = models.CharField(max_length=10)
-    branch = models.CharField(max_length=10, choices=BRANCH_CHOICES)
+    branch = models.CharField(max_length=10, choices=BRANCH_CHOICES, default="BA")
 
     def __str__(self):
         return self.first_name + " " + self.last_name
